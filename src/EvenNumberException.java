870e9dbd4a915d4bcae3e180959825a09c8ea763
Yuriy Musienko <97musienko@gmail.com>
2021-04-10 14:24:00 +0300

    tmshw8

---
+++ src/EvenNumberException.java
@@ -0,0 +1,12 @@
+public class EvenNumberException extends Exception {
+    public EvenNumberException() {
+    }
+
+    public EvenNumberException(String message) {
+        super(message);
+    }
+
+    public EvenNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
